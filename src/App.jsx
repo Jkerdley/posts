475aa22465cea7d6092442bf6dc71720586d9902
Jkerdley <jk-erley@yandex.ru>
2025-04-03 20:02:38 +0700

    init: Инициализировал проект

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <main className="mainpage">
+            <p>Click on the Vite and React logos to learn more</p>
+        </main>
+    );
+}
+
+export default App;
